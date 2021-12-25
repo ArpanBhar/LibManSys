fda7b288d3f6678520de6429ef4f8614d8490733
ArpanBhar <96098236+ArpanBhar@users.noreply.github.com>
2021-12-25 19:02:49 +0530

    Update ComputerPr.py

--- ComputerPr.py
+++ ComputerPr.py
@@ -2,7 +2,7 @@ from tkinter import *
 from datetime import *
 from os import system,chdir
 import threading
-directory = __file__.split("\\");directory.remove("ComputerPr.py");chdir("\\".join(directory))
+chdir("\\".join(__file__.split("\\")[0:-1]))
 root = Tk()
 root.title("Library Management System")
 root.geometry("360x480")
